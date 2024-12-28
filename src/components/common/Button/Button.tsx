63b3bcd98a0af576480745d80a08414616026ace
KevinLatino <latinokevin9@gmail.com>
2024-12-28 13:25:56 -0600

    minor changes

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -8,7 +8,7 @@ export interface ButtonProps {
 
 const typeClasses: Record<string, string> = {
   primary: "bg-light-primary text-white",
-  outline: "border-1 border-gray-300 bg-transparent text-gray-700 text-white",
+  outline: "border border-gray-300 bg-transparent text-gray-700 text-white",
   neutral: "bg-light-neutral text-light",
   light: "bg-white text-black"
 };
@@ -19,7 +19,7 @@ export const Button: React.FC<ButtonProps> = ({
   onClick,
 }) => {
 
-  const baseClasses = "cursor-pointer text-center rounded-full font-bold text-center w-[180px] h-[23] py-[15px] px-[30px]";
+  const baseClasses = "text-button1 cursor-pointer text-center rounded-full font-bold w-[180px] h-[23] py-[15px] px-[30px]";
   const appliedClasses = `${baseClasses} ${type ? typeClasses[type] : ''}`;
 
   return (
