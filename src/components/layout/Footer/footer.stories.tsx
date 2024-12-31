e666a59452729fc790a8a393ae9646c686eea700
villarley <115122095+Villarley@users.noreply.github.com>
2024-12-31 00:14:07 -0600

    Footer added

---
+++ src/components/layout/Footer/footer.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Footer from './footer'; 
+
+const meta: Meta<typeof Footer> = {
+  title: 'Components/Footer',
+  component: Footer,
+  parameters: {
+    layout: 'fullscreen', 
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Footer>;
+
+export const Default: Story = {
+  args: {
+  },
+};
