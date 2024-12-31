c2a37cc53f5dfbd1f14686f3cc2c5e0a442125f5
villarley <115122095+Villarley@users.noreply.github.com>
2024-12-30 23:44:12 -0600

    Added Facebook icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,5 +2,6 @@ export { default as Filter } from './Filter';
 export { default as GitHub } from './GitHub';
 export { default as Instagram } from './Instagram';
 export { default as Twitter } from './Twitter';
+export { default as Facebook } from './Facebook';
 // Add other icons here as needed
 // export { default as AnotherIcon } from './AnotherIcon'; 
