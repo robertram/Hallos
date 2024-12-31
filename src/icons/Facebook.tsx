b35ed820e33fa86b83d5ba85f66678acfd114a93
villarley <115122095+Villarley@users.noreply.github.com>
2024-12-30 23:39:58 -0600

    Icons Added

---
+++ src/icons/Facebook.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Facebook: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    width="9"
+    height="15"
+    viewBox="0 0 9 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M2.94093 14.0956V7.68548H0.773438V5.18733H2.94093V3.34501C2.94093 1.20702 4.25301 0.0428467 6.16939 0.0428467C7.08736 0.0428467 7.87631 0.110865 8.10623 0.141267V2.37559L6.77711 2.37619C5.73487 2.37619 5.53307 2.86908 5.53307 3.59237V5.18733H8.01871L7.69507 7.68548H5.53307V14.0956H2.94093Z"
+      fill="#F9FAFB"
+    />
+  </svg>
+);
+
+export default Facebook;
