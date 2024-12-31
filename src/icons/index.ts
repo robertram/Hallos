c8e1a092135594a01421156b6938e489337990a9
villarley <115122095+Villarley@users.noreply.github.com>
2024-12-30 23:41:48 -0600

    Export icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,6 @@
 export { default as Filter } from './Filter';
+export { default as GitHub } from './GitHub';
+export { default as Instagram } from './Instagram';
+export { default as Twitter } from './Twitter';
 // Add other icons here as needed
 // export { default as AnotherIcon } from './AnotherIcon'; 
