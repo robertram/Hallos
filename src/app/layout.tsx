7ce2a93cd6ffea39d6baed7dc6e4f7278dc4c9dd
Robert <rsft6000@gmail.com>
2025-01-02 23:25:27 -0600

    main: footer rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import "./globals.css";
-import { Header } from "@/components/layout/Header/Header";
 import Footer from "@/components/layout/Footer/Footer";
 
 const geist = Geist({
