c54d59ec2efdae6cda93f652ddb3a7356c69d711
Derian <dmrodriguez2000@gmail.com>
2025-01-02 19:41:49 -0600

    feat: add EventCard stories

---
+++ src/components/EventCard/EventCard.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { EventCard } from "./EventCard";
+
+const meta = {
+  title: "Components/EventCard",
+  component: EventCard,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof EventCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const UpcomingEvent: Story = {
+  args: {
+    image: "/images/vueltos-un-colocho.png",
+    title: "Vueltos un colocho",
+    date: "1735833600000",
+    location: "Teatro El Escenario",
+    price: "43.33",
+  },
+};
+
+export const PastEvent: Story = {
+  args: {
+    image: "/images/vueltos-un-colocho.png",
+    title: "Vueltos un colocho",
+    date: "1735833600000",
+    location: "Teatro El Escenario",
+  },
+};
