f99fd1165b824aec0af9b3cf9a0f1fe7e2a2a74a
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-01-26 23:46:24 -0600

    fix: clean up comments and maintain tooltipInfo prop in EventTicket component

--- src/components/common/EventTicket/EventTicket.tsx
+++ src/components/common/EventTicket/EventTicket.tsx
@@ -1,16 +1,16 @@
 type EventTicketProps = {
   name: string;
   image: string;
-  tooltipInfo?: string; // Agregamos la propiedad opcional para el tooltip
+  tooltipInfo?: string;
 };
 
 export const EventTicket = ({ name, tooltipInfo }: EventTicketProps) => {
   return (
     <div 
       className="bg-black text-white rounded-lg shadow-md w-40 p-2 flex flex-col items-center border-2 border-gray-300" // Añadido border
-      title={tooltipInfo} // Tooltip agregado
+      title={tooltipInfo}
     >
-      {/* Imagen dinámica */}
+      {/* dynamic image */}
       <div className="w-full h-48 bg-gray-200 rounded-lg overflow-hidden">
         <img
           src={"https://37cab6b984fd76206e94c796d0b1a00c.ipfscdn.io/ipfs/bafybeic3mambq274fafn2o4hcb4swdzm7ntezh2zlfoa644sxfll6ifb54/1.png"} // Cambié la URL a la variable image
@@ -19,7 +19,7 @@ export const EventTicket = ({ name, tooltipInfo }: EventTicketProps) => {
         />
       </div>
 
-      {/* Nombre del ticket */}
+      {/* Ticket name */}
       <div className="mt-2 text-center">
         <p className="text-sm font-medium">{name}</p>
       </div>
