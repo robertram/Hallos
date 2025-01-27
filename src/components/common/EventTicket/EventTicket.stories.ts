da6145e3c3866c0777a2d384569ec0082d54ac74
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-01-26 23:46:17 -0600

    fix: remove commented tooltipInfo in EventTicket stories

--- src/components/common/EventTicket/EventTicket.stories.ts
+++ src/components/common/EventTicket/EventTicket.stories.ts
@@ -17,6 +17,6 @@ export const Default: Story = {
   args: {
     name: "Name of the ticket",
     image: "https://37cab6b984fd76206e94c796d0b1a00c.ipfscdn.io/ipfs/bafybeic3mambq274fafn2o4hcb4swdzm7ntezh2zlfoa644sxfll6ifb54/1.png",
-    tooltipInfo: "This ticket is a digital collectible", // Tooltip agregado
+    tooltipInfo: "This ticket is a digital collectible",
   },
 };
